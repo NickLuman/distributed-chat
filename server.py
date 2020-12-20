1b5f6165bab65b1412f4f9409a458dcf938ff5fa
Nikita Lupenko <nicklupman@gmail.com>
2020-12-20 18:46:59 +0300

    add polling

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import socket
+import select
 from multiprocessing import Process
 
 class Server(Process):
@@ -19,22 +20,31 @@ class Server(Process):
     def run(self):
         print("Server is running...")
 
+        poller_socket = select.poll()
+        poller_socket.register(self.sock, select.POLLIN)
+
+        poller_timer = select.poll()
+        poller_timer.register(self.timer_point, select.POLLIN)
+
         while 1:
-            data = None
-            try:
+            data, timer_buffer = None, None
+            
+            socket_Event = poller_socket.poll(1000)
+            for descriptor, Event in socket_Event:
                 data, address = self.sock.recvfrom(1024)
-            except socket.error:
-                pass
-            finally:
+            
+            timer_Event = poller_timer.poll(1000)
+            for descriptor, Event in timer_Event:
                 timer_buffer = self.timer_point.recv()
 
-                if data:
-                    print("address: {0}, {1}; message: {2} ---- {3}".format(address[0], address[1], str(timer_buffer), data.decode('utf-8')))
 
-                    message_for_all = ("{0} {1}".format(
-                        str(timer_buffer), data.decode('utf-8'))).encode('utf-8')
+            if data and timer_buffer:
+                print("address: {0}, {1}; message: {2} ---- {3}".format(address[0], address[1], str(timer_buffer), data.decode('utf-8')))
 
-                    if address not in self.clients:
-                        self.clients.append(address)
-                    for client in self.clients:
-                        self.sock.sendto(message_for_all, client)
+                message_for_all = ("{0} {1}".format(
+                    str(timer_buffer), data.decode('utf-8'))).encode('utf-8')
+
+                if address not in self.clients:
+                    self.clients.append(address)
+                for client in self.clients:
+                    self.sock.sendto(message_for_all, client)
